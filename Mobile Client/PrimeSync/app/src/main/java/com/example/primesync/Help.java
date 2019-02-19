6210f2e5177b17c1378f5b99a7c8556c7fbfd320
Cameron_MacLean <cameron.maclean@kcl.ac.uk>
2019-02-19 15:33:14 +0000

    Upload final mobile app
    Co-Authored-By: mandeep-singh-rai <mandeep-singh-rai@users.noreply.github.com>

---
+++ Mobile Client/PrimeSync/app/src/main/java/com/example/primesync/Help.java
@@ -0,0 +1,28 @@
+package com.example.primesync;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Help extends Fragment {
+
+
+    public Help() {
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.helppage, container, false);
+    }
+
+
+
+
+
+}
