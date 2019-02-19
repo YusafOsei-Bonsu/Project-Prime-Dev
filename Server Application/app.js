9e2e9ce09152682d492a45f19eca6e7b93fd1887
Saloni Handuja <SaloniH@users.noreply.github.com>
2019-02-19 13:21:14 +0000

    Update app.js
    Adding Comments

--- Server Application/app.js
+++ Server Application/app.js
@@ -1,8 +1,12 @@
+//Middleware between server and the routes
+
+//Express for building APIs
 const express = require('express')
 const app = express()
 
 const fileRoute = require('./api/routes/files')
 
+//Routing to files.js to handle requests
 app.use('/files', fileRoute)
 
 module.exports = app
