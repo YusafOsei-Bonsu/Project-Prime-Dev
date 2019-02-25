7c8da0f893a6f813dedc7204d9a3fd9dddd26fc0
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2019-02-25 15:03:18 +0000

    Update index.js
    Inserting the MongoURI

--- Desktop Client/src/index.js
+++ Desktop Client/src/index.js
@@ -1,7 +1,8 @@
 var mongo = require('mongodb').MongoClient;
+const mongoURI = 'mongodb://projectPrime:projectPrime@projectprime-shard-00-00-wreg9.mongodb.net:27017,projectprime-shard-00-01-wreg9.mongodb.net:27017,projectprime-shard-00-02-wreg9.mongodb.net:27017/test?ssl=true&replicaSet=ProjectPrime-shard-0&authSource=admin&retryWrites=true';
 
 /* 
-//mongo.connect(MongoDB URI goes here, function(error, db) {
+//mongo.connect(mongoURI, function(error, db) {
   if(!error) {
     alert("Connected to MongoDB database woo!");
   } else {
