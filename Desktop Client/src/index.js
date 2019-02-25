ca3047c577c4e9b6c2f372971fdf7c4b2852e6bf
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2019-02-25 14:54:38 +0000

    Create index.js
    This file will form a connection between the desktop client and the project prime mongoDB database

---
+++ Desktop Client/src/index.js
@@ -0,0 +1,11 @@
+var mongo = require('mongodb').MongoClient;
+
+/* 
+//mongo.connect(MongoDB URI goes here, function(error, db) {
+  if(!error) {
+    alert("Connected to MongoDB database woo!");
+  } else {
+    alert("Something went wrong bro.. this sucks bro.");
+  }
+});
+*/
