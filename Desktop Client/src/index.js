e60e9f601cce947d9935912de610bd3e285eec67
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2019-02-25 15:38:01 +0000

    Update index.js

--- Desktop Client/src/index.js
+++ Desktop Client/src/index.js
@@ -2,7 +2,7 @@ var mongo = require('mongodb').MongoClient;
 const mongoURI = 'mongodb://projectPrime:projectPrime@projectprime-shard-00-00-wreg9.mongodb.net:27017,projectprime-shard-00-01-wreg9.mongodb.net:27017,projectprime-shard-00-02-wreg9.mongodb.net:27017/test?ssl=true&replicaSet=ProjectPrime-shard-0&authSource=admin&retryWrites=true';
 
 /* 
-//mongo.connect(mongoURI, function(error, db) {
+mongo.connect(mongoURI, function(error, db) {
   if(!error) {
     alert("Connected to MongoDB database woo!");
   } else {
