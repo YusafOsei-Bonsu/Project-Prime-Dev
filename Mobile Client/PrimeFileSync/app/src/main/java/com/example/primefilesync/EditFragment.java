d9f650dbf86e5b97cc32bd7d635f95fafd23574e
Cameron_MacLean <cameron.maclean@kcl.ac.uk>
2019-03-10 17:50:33 +0000

    Latest Application
    Base completed fully - missing main function

---
+++ Mobile Client/PrimeFileSync/app/src/main/java/com/example/primefilesync/EditFragment.java
@@ -0,0 +1,23 @@
+package com.example.primefilesync;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EditFragment extends Fragment {
+
+    public EditFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.edit_page, container, false);
+    }
+
+
+}
---
+++ Mobile Client/PrimeFileSync/app/src/main/java/com/example/primefilesync/HomeFragment.java
@@ -0,0 +1,29 @@
+package com.example.primefilesync;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.home_page, container, false);
+        //for list view
+
+    }
+
+    ListView simpleListView;
+    String fileList[] = {"test","test2"};
+
+
+}
