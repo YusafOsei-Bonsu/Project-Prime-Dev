d0d63e09b0e27fc2a9bb7eb558b3fdf29181eac6
Cameron_MacLean <cameron.maclean@kcl.ac.uk>
2019-03-12 15:52:08 +0000

    Base App completed
    All pages and navigation working fine, still waiting on server connection.
    
    Co-Authored-By: mandeep-singh-rai <mandeep-singh-rai@users.noreply.github.com>

--- Mobile Client/PrimeFileSync/app/src/main/java/com/example/primefilesync/HomeFragment.java
+++ Mobile Client/PrimeFileSync/app/src/main/java/com/example/primefilesync/HomeFragment.java
@@ -7,7 +7,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class HomeFragment extends Fragment {
+
+    //test
+    //create arrays for the listview
+    String[] nameArray = {"test1","test2","test3","test4","test5","test6","test7","test8","test9","test10"};
+    String[] authorArray = {"test11","test22","test33","test44","test55","test66","test77","test88","test99","test1010"};
+    Integer[] imageArray = {R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery,R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery,R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery,R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery,R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery};
+    ListView listView;
+    View rootview;
+
+    //test ends
 
     public HomeFragment() {
         // Required empty public constructor
@@ -16,14 +32,47 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home_page, container, false);
-        //for list view
+
+        /*for list view (url = server url) - starts by connecting to server and ends by disconnecting
+        try {
+            URL url = new URL("mongodb://projectPrime:projectPrime@projectprime-shard-00-00-wreg9.mongodb.net:27017,projectprime-shard-00-01-wreg9.mongodb.net:27017,projectprime-shard-00-02-wreg9.mongodb.net:27017/test?ssl=true&replicaSet=ProjectPrime-shard-0&authSource=admin&retryWrites=true");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+
+                //what to do from the server URL (here fill the list maybe)
+
+
+
+                //InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                //readStream(in);
+            //} catch (IOException ex) {
+             //   urlConnection.disconnect();
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (IOException ex2) {
+            System.out.print("Cant connect to server");
+        }
+
+           */
+
+        rootview=inflater.inflate(R.layout.home_page,container, false);
+        init();
+        return rootview;
+
+
+
+
 
     }
 
-    ListView simpleListView;
-    String fileList[] = {"test","test2"};
+    public void init(){
+
+        CustomListAdapter listvieweg = new CustomListAdapter(this.getActivity() , nameArray, authorArray, imageArray);
+        listView = (ListView) rootview.findViewById(R.id.simpleListView);
+        listView.setAdapter(listvieweg);
+
+    }
 
 
 }
