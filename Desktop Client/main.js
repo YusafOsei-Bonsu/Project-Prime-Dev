6323c8ae47d7c733587668b1cca58ab9f4299f87
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-03-14 20:22:00 +0000

    Renamed HTML file
    Renamed the file from upload to index

--- Desktop Client/main.js
+++ Desktop Client/main.js
@@ -15,7 +15,7 @@ function createWindow () {
   window = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  window.loadFile('src/upload.html');
+  window.loadFile('src/index.html');
   
   //AJAX request server for existing files
   var xhr = new XMLHttpRequest();
