8b65ac46dad8ee14558c1b94a485e3dd9ad99540
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-03-18 20:11:30 +0000

    Create server.js
    Adding the server responsible for passing http requests to the database

---
+++ Desktop Client/server.js
@@ -0,0 +1,16 @@
+/** 
+ * Main server file to run the server
+ * */
+
+// Http protocol for requests
+const http = require('http');
+
+//Adding dependency to the middleware
+const app = require('./src/app.js');
+
+//Currently using localhost port "3003" - Arbitrary port
+const port = process.env.PORT || 3003
+
+const server = http.createServer(app);
+
+server.listen(port);
