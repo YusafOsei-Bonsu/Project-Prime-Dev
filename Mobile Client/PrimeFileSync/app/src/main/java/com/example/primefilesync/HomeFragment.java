292c62e586e2dc4f2c4c0597079ec0b05b460492
Cameron_MacLean <cameron.maclean@kcl.ac.uk>
2019-03-20 16:24:13 +0000

    Revert "Base App with additional json requests"
    This reverts commit 92f2c91ac0893bcf371f84e402e58b390e4c98bc.

--- Mobile Client/PrimeFileSync/app/src/main/java/com/example/primefilesync/HomeFragment.java
+++ Mobile Client/PrimeFileSync/app/src/main/java/com/example/primefilesync/HomeFragment.java
@@ -7,17 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.*;
-import java.net.*;
-
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,9 +15,15 @@ import java.net.URL;
 
 public class HomeFragment extends Fragment {
 
+    //test
+    //create arrays for the listview
+    String[] nameArray = {"test1","test2","test3","test4","test5","test6","test7","test8","test9","test10"};
+    String[] authorArray = {"test11","test22","test33","test44","test55","test66","test77","test88","test99","test1010"};
+    Integer[] imageArray = {R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery,R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery,R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery,R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery,R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery};
     ListView listView;
     View rootview;
 
+    //test ends
 
     public HomeFragment() {
         // Required empty public constructor
@@ -71,37 +66,7 @@ public class HomeFragment extends Fragment {
 
     }
 
-
-
     public void init(){
-        System.out.println("test1");
-        RequestQueue queue = Volley.newRequestQueue(this.getContext());
-        String url = "http:/localhost/files";
-
-            JsonObjectRequest jsonArrayRequest = new JsonObjectRequest(Request.Method.GET, url,null, new Response.Listener<JSONObject>() {
-
-                @Override
-                public void onResponse(JSONObject response) {
-                    //System.out.println("response: "+response.toString());
-                    System.out.println("test2");
-                }
-
-
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    System.out.println("error 1");
-                }
-            });
-
-       queue.add(jsonArrayRequest);
-        //make sure can connect to server,
-
-        System.out.println("test3");
-        String[] nameArray = {"test1","test2","test3","test4","test5","test6","test7","test8","test9","test10"};
-        String[] authorArray = {"test11","test22","test33","test44","test55","test66","test77","test88","test99","test1010"};
-        Integer[] imageArray = {R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery,R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery,R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery,R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery,R.drawable.ic_menu_gallery, R.drawable.ic_menu_gallery};
-
 
         CustomListAdapter listvieweg = new CustomListAdapter(this.getActivity() , nameArray, authorArray, imageArray);
         listView = (ListView) rootview.findViewById(R.id.simpleListView);
